7fcc40e74efb381d31617e44a282b1f05b93d2c5
Curt Lewis <curt.lewis2606@gmail.com>
2020-07-12 17:24:58 -0400

    fixed indentation

--- math/mod.s
+++ math/mod.s
@@ -29,24 +29,24 @@
 */
 
         .arch   armv8-a
-				.file   "mod.s"
-				.text
-				.global mod 
-				.type   mod, %function
+        .file   "mod.s"
+        .text
+        .global mod 
+        .type   mod, %function
 
 mod:
         // if denominator is 0 return with a value of 0
-				cmp     x1, #0
-				csel    x0, x1, x0, eq  // if denom = 0, move 0 into return register
-				                        // otherwise keep numerator in x0 
-				b.eq    return          // if denom = 0 return
+        cmp     x1, #0
+        csel    x0, x1, x0, eq  // if denom = 0, move 0 into return register
+                                // otherwise keep numerator in x0 
+        b.eq    return          // if denom = 0 return
 
         sdiv    x2, x1, x0      // x2 = x1/x0 ( q = d/n )
-				mul     x3, x2, x0      // x3 = x2*x0 ( n*q )
-				sub     x0, x1, x3      // r = d - n*q
-				cmp     x0, #0
-				neg     x1, x0
-				csel    x0, x1, x0, lt   // if x0 < 0 : x1 ? x0
+        mul     x3, x2, x0      // x3 = x2*x0 ( n*q )
+        sub     x0, x1, x3      // r = d - n*q
+        cmp     x0, #0
+        neg     x1, x0
+        csel    x0, x1, x0, lt   // if x0 < 0 : x1 ? x0
 
 return:
         ret
